89b5c964117916b3cfdcb8016fc2bef5666af873
Andy Sims <andy.sims@zendesk.com>
2022-08-09 16:06:05 +1000

    Embed robot in react structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,46 @@
-import React from 'react';
+import React, { PropsWithRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Facing, Robot } from './lib/Robot'
+
+const Grid = () => {
+  const gridStructure = Array(5).fill(0).map(row => Array(5).fill(0))
+
+  return (
+    <>
+    {
+      gridStructure
+      .map((row, index) => {
+        <div key={index}>
+          { row.map((col, index) => <span key={`col${index}`}>{col} </span>) }
+        </div>
+      })
+    }
+    </>
+  )
+}
+
+const Hello = () => {
+  return <p>hello world</p>
+}
 
 function App() {
+  const robot = Robot.place(0, 0, Facing.North)
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Robot was placed on load of this page.
+        </p>
+  
+  
+        <Grid />
+        <Hello />
+
+        <p>
+          Report: { robot.report() }
         </p>
         <a
           className="App-link"
@@ -24,3 +56,11 @@ function App() {
 }
 
 export default App;
+
+// TODOs:
+// Make it into React style, so that robot state is managed in the app
+
+// render the robot on a grid (visually)
+
+// allow user to send commands to robot
+
---
+++ src/lib/Robot.ts
@@ -0,0 +1,82 @@
+import { facingDirection, isOnTheBoard } from "./robotUtils";
+
+export enum Facing {
+  North,
+  East,
+  South,
+  West,
+  __LENGTH,
+}
+
+export class Robot {
+  x: number;
+  y: number;
+  f: Facing;
+
+  static place(x: number, y: number, f: Facing) {
+    if (!Object.values(Facing).includes(f)) {
+      throw new Error("invalid facing");
+    }
+    if (!isOnTheBoard(x) || !isOnTheBoard(y)) {
+      throw new Error(`invalid ${!isOnTheBoard(x) ? "x" : "y"}`);
+    }
+    return new Robot(x, y, f);
+  }
+
+  constructor(x: number, y: number, f: Facing) {
+    this.x = x;
+    this.y = y;
+    this.f = f;
+  }
+
+  left(): void {
+    if (this.f === 0) {
+      this.f = Facing.__LENGTH - 1;
+      return;
+    }
+
+    this.f -= 1;
+  }
+
+  // Do we want to "re-place"
+  // place() {}
+
+  right(): void {
+    if (this.f === Facing.__LENGTH - 1) {
+      this.f = 0;
+      return;
+    }
+
+    this.f += 1;
+  }
+
+  move(): void {
+    // @TODO: Do we want to throw an error when the robot wants
+    // to move in a bad direction? I would.
+
+    // @TODO: Is this ok, or do we want a more sophisticated
+    // method to report the position, see which moves are not allowed
+    // and not allow?
+
+    switch (this.f) {
+      case Facing.West:
+        if (this.x > 0) this.x -= 1;
+        break;
+      case Facing.South:
+        if (this.y > 0) this.y -= 1;
+        break;
+      case Facing.North:
+        if (this.y < 4) this.y += 1;
+        break;
+      case Facing.East:
+        if (this.x < 4) this.x += 1;
+        break;
+      default:
+        break;
+    }
+  }
+
+  report(): string {
+    return `${this.x}, ${this.y}, ${facingDirection(this.f)}`;
+  }
+}
