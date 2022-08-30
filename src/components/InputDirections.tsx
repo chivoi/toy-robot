c5508141253cac25ddfb0d13064e04a271bb3bfb
Ana Lastoviria <ana.lastoviria@zendesk.com>
2022-08-30 10:55:09 +1000

    fix center alignment of the thing

--- src/components/InputDirections.tsx
+++ src/components/InputDirections.tsx
@@ -5,7 +5,7 @@ export const InputDirections = () => {
     return (
         <div className='commandContainer'>
             <h3>Available commands:</h3>
-            <ul style={{ listStyle: "none", width: "50%", textAlign: "left" }}>
+            <ul style={{ listStyle: "none", textAlign: "left" }}>
                 <li><i><b>PLACE x y Facing</b></i> - places the robot on the board</li>
                 <li><i><b>LEFT</b></i> - totates the robot 90 degrees left</li>
                 <li><i><b>RIGHT</b></i> - totates the robot 90 degrees right</li>
