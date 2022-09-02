2b344e85fb4f1242d366f021de7550fd0767061a
Ana Lastoviria <ana.lastoviria@zendesk.com>
2022-09-02 16:02:35 +1000

    more frontend I can't stop

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import _ from 'lodash';
+import _, { size } from 'lodash';
 import { Icon } from '@iconify/react';
 
 import React, { useState } from 'react';
@@ -6,18 +6,20 @@ import { InputDirections } from './components/InputDirections';
 import { Grid } from './components/Grid';
 import ErrorAlert from './components/ErrorAlert';
 import { RobotSession, RobotCoordinates } from './lib/robot/RobotSession';
+import { Facing, Robot } from './lib/robot/Robot';
 
 const BoardSize = 5
 
 // TODOs
 // - Bug when going back, turn left/right doesn't seem to work
+// - Bug: when use move before place, can not go back, says no earlier history
 // - Error messages
 
 function App(): JSX.Element {
   const [command, setCommand] = useState<string>('');
   const [error, setError] = useState<Error | null>(null);
   const [session] = useState(new RobotSession(BoardSize))
-  const [robot, setRobot] = useState<RobotCoordinates>(session.current())
+  const [robot, setRobot] = useState(session.current())
 
   const handleCommandInput = (event: React.ChangeEvent<HTMLInputElement>) => {
     const enteredCommand = event.target.value;
@@ -52,24 +54,31 @@ function App(): JSX.Element {
   }
 
   return (
-    <div className="App">
-      {error && ErrorAlert(error.message)}
-      <h1 style={{ color: "#C54D45" }}>Move me <Icon icon="mdi:robot-happy" inline={true} /></h1>
+    <>
+      <div className="App">
+        {error && ErrorAlert(error.message)}
+        <h1 style={{ color: "#C54D45" }}>M<Icon icon="mdi:robot-happy" inline={true} />ve me</h1>
+        <div className="GridRulesContainer">
+          <div className="GridInputButtonsContainer">
+            <Grid boardSize={BoardSize} x={robot.x} y={robot.y} f={robot.f} />
 
-      <Grid boardSize={BoardSize} x={robot.x} y={robot.y} f={robot.f} />
+            <input
+              value={command}
+              onChange={handleCommandInput}
+              placeholder="place 0 0 north"
+              className="input"
+            />
 
-      <input
-        value={command}
-        onChange={handleCommandInput}
-        placeholder="place 0 0 north"
-        className="input"
-      />
+            <p>Report: {robot.x}, {robot.y}, {Facing[robot.f]}, rotor {robot.rotorOn ? "on" : "off"}</p>
 
-      <button onClick={handleClick}>Execute!</button>
-      <button onClick={executeBack}>Back!</button>
-
-      <InputDirections />
-    </div>
+            <button onClick={handleClick}>Execute!</button>
+            <button onClick={executeBack}>Back!</button>
+          </div>
+          <InputDirections />
+        </div>
+      </div>
+      <Icon icon="mdi:robot-happy" style={{ fontSize: '15em', color: 'white', opacity: '0.6', position: 'absolute', left: '0.2em', bottom: '0.0001em', zIndex: '-1' }} />
+    </>
   );
 }
 
